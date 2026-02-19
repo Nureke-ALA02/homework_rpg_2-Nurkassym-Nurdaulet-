63f88d111dee371f71562a0fb97def5469370339
Nureke-ALA02 <nurdaulet.nurkassym@narxoz.kz>
2026-02-19 08:08:20 +0500

    add new class name is flamebreath

---
+++ src/com/narxoz/rpg/combat/FlameBreath.java
@@ -0,0 +1,10 @@
+package com.narxoz.rpg.combat;
+
+public class FlameBreath implements Ability {
+    @Override public String getName(){
+        return "Flame Breath";
+    }
+    @Override public int getDamege(){
+        return 50;
+    }
+}
